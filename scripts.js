e1387073a7fc898412c869dd3d040b81f8090a10
hianmateus <hianmateus1@gmail.com>
2024-03-20 23:59:18 -0300

    falta os scripts

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const Mask1 = document.querySelector('.Mask1')
+const Mask2 = document.querySelector('.Mask2')
+
+function OpenMask() {
+    Mask1.style.transform = `translateX(-105%)`
+    Mask2.style.transform = `translateX(200%)`
+
+}
+
+OpenMask()
